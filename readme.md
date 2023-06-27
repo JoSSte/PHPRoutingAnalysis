9aae733739a126a8dba865af641d4e9723ac55f6
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2023-06-27 21:43:18 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# What is it, precious?
+This is a test to see if a single routing file is more effective than a webserver based config like apache's mod_rewrite and .htaccess
---
+++ src/singlefile/index.php
@@ -0,0 +1,24 @@
+<?php
+// https://www.freecodecamp.org/news/how-to-build-a-routing-system-in-php/
+
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '../views/';
+
+switch ($request) {
+    case '':
+    case '/':
+        require __DIR__ . $viewDir . 'home.php';
+        break;
+
+    case '/views/users':
+        require __DIR__ . $viewDir . 'users.php';
+        break;
+
+    case '/contact':
+        require __DIR__ . $viewDir . 'contact.php';
+        break;
+
+    default:
+        http_response_code(404);
+        require __DIR__ . $viewDir . '404.php';
+}
