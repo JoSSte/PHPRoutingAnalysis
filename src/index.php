d888cd78ac56408bf8a6fa30befa5d62be3b113f
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2023-06-28 21:39:23 +0200

    fixing initial config

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>dummy index</title>
+</head>
+
+<body>
+<?php
+echo "PHP works";
+?>
+</body>
+
+</html>
--- src/singlefile/index.php
+++ src/singlefile/index.php
@@ -2,19 +2,22 @@
 // https://www.freecodecamp.org/news/how-to-build-a-routing-system-in-php/
 
 $request = $_SERVER['REQUEST_URI'];
-$viewDir = '../views/';
+$viewDir = '/../views/';
+
+//dump requestt_uri for debugging
+header("X-request: " . $request);
 
 switch ($request) {
-    case '':
-    case '/':
+    case '/singlefile':
+    case '/singlefile/':
         require __DIR__ . $viewDir . 'home.php';
         break;
 
-    case '/views/users':
+    case '/singlefile/views/users':
         require __DIR__ . $viewDir . 'users.php';
         break;
 
-    case '/contact':
+    case '/singlefile/contact':
         require __DIR__ . $viewDir . 'contact.php';
         break;
 
